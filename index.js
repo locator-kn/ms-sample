a8f7c9430275066c53a6e0de9d894ca7f3f2c815
SteffenGorenflo <SteffenGorenflo@gmail.com>
2015-12-22 17:14:42 +0100

    remove all seneca related plugins

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
 'use strict';
-const seneca = require('seneca')();
-const file = require('./lib/module');
-const database = require('./lib/database');
 
 require('dotenv').config({path: '../.env'});
 
-// select desired transport method
-const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
-const patternPin = 'role:file';
-
-// init database and then seneca and expose functions
-database.connect()
-    .then(() => {
-        seneca
-            .use(transportMethod + '-transport')
-            .add(patternPin + ',cmd:get', file.getImage)
-            .listen({type: transportMethod, pin: patternPin});
     });
