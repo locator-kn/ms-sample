a77092419440e46623182554b23155c92a8cb77c
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-01-14 19:30:11 +0100

    implement correct signature when sending slack errors

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ Glue.compose(manifest, {relativeTo: __dirname}, (err, server) => {
     server.route(routes);
 
     server.ext('onRequest', (request, reply) => {
-        //slack.sendSlackError(request.headers);
+        //slack.sendSlackError(process.env['SLACK_ERROR_CHANNEL'], request.headers);
         console.log(request.headers);
         reply.continue();
     });
