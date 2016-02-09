abc961dfe8f4cf2f282f2552b43301b61c789564
SteffenGorenflo <SteffenGorenflo@gmail.com>
2016-02-09 11:27:48 +0100

    improve sample code

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 'use strict';
+
+const path = require('path');
+const pwd = path.join(__dirname, '..', '/.env');
+require('dotenv').config({path: pwd});
+
 const seneca = require('seneca')();
 const myModule = require('./lib/module');
 const database = require('./lib/database');
 
-require('dotenv').config({path: '../.env'});
 
 // select desired transport method
-const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
-const patternPin = 'role:user';
+//const transportMethod = process.env['SENECA_TRANSPORT_METHOD'] || 'rabbitmq';
+
+
+const patternPin = 'role:user';  // TODO: Change role
 
 // init database and then seneca and expose functions
 database.connect()
     .then(() => {
         seneca
-            .use(transportMethod + '-transport')
-            .add(patternPin + ',cmd:login', myModule.doSomething)
-            .add(patternPin + ',cmd:else', myModule.doSomethingElse)
-            .listen({type: transportMethod, pin: patternPin});
+            .add(patternPin + ',cmd:do', myModule.do)
+            .add(patternPin + ',cmd:do,entity:something', myModule.doSomething)
+            .add(patternPin + ',cmd:do,entity:else', myModule.doSomethingElse)
+            .listen({type: 'tcp', port: 7001, pin: patternPin});
     });
